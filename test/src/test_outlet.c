0b1774f8d855359144575a1c9923aacc31a4b5c8
VARRTIX <zhaow@jnsec.in>
2024-06-06 16:42:36 +0800

    clean: ugly test code

--- test/src/test_outlet.c
+++ test/src/test_outlet.c
@@ -102,19 +102,16 @@ void test_fx_outlet(void **state) {
 
   assert_ptr_not_equal(ioctx = fx_ioctx_new(), NULL);
   assert_int_equal(fx_ioctx_set(ioctx, FX_IOPT_OUTLET, outlet), 1);
-  assert_ptr_not_equal(
-      fx_keychain_create2(ioctx, FX_BM_KEYCHAIN, 4, fx_bytes_new("devid", 5),
-                          fx_bytes_new("provid", 6), fx_bytes_new("kmcid", 5),
-                          fx_bytes_new("aucid", 5)),
-      NULL);
+  assert_ptr_not_equal(kc = fx_keychain_create2(
+                           ioctx, FX_BM_KEYCHAIN, 4, fx_bytes_new("devid", 5),
+                           fx_bytes_new("provid", 6), fx_bytes_new("kmcid", 5),
+                           fx_bytes_new("aucid", 5)),
+                       NULL);
+  port_name = fx_keychain_encode(kc);
+  assert_int_equal(fx_bytes_check(&port_name), 1);
+  print_message("[encoded keychain][BM] %s\n", port_name.ptr);
+  fx_bytes_free(&port_name);
   fx_keychain_destroy(kc);
-
-  // assert_ptr_not_equal(
-  //     auc = fx_auc_keygen(
-  //         ioctx, fx_bytes_new((uint8_t *)app_name, strlen(app_name)),
-  //         fx_bytes_new("Jiangsu", 7),
-  //         fx_bytes_new((uint8_t *)conta_name, strlen(conta_name))),
-  //     NULL);
 
   fx_ioctx_free(ioctx);
   fx_outlet_free(outlet);
